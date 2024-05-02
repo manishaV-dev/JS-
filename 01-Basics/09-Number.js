b3342e563206add38b7917c36973045701102bb4
Manisha Varma <manishavkapex1@gmail.com>
2024-05-02 11:29:55 +0000

    More Info About Number

--- 01-Basics/09-Number.js
+++ 01-Basics/09-Number.js
@@ -1,3 +1,5 @@
+// The JavaScript interpreter works from left to right.
+
 const number = 100;
 console.log(number)
 
@@ -51,3 +53,84 @@ let max = 20;
 let min = 10
 // + 1 is used to avoid 0
 console.log(Math.floor(Math.random() * (max-min + 1 )) + min)
+
+
+// ============================================ Numbers ===================================
+
+
+// 1. JavaScript will try to convert strings to numbers in all numeric operations: (/, *, -)
+let a = '100'
+let b = '10'
+console.log(a/b); // 10 -- type will become Number
+console.log(a*b); // 1000 -- type will become Number
+console.log(a-b); // 90 -- type will become Number
+console.log(a+b); // 10010 -- type still String as + is used to concatenate a string
+
+
+/*
+
+NaN is a JavaScript reserved word indicating that a number is not a legal number.
+        Trying to do arithmetic with a non-numeric string will result in NaN (Not a Number):
+        NaN is a number: typeof NaN returns number:
+
+*/
+
+let x = 100 / "Apple";
+console.log(x) ; //NaN
+
+// however if the string is numeric, the result will be a number:
+
+let z = 100 / "10";
+console.log(z); // 10
+
+
+/*
+
+Infinity (or -Infinity) is the value JavaScript will return if you calculate a number outside the largest possible number.
+        Division by 0 (zero) also generates Infinity:
+        Infinity is a number: typeof Infinity returns number.
+
+
+
+        Notes: 
+                1.  Never write a number with a leading zero (like 07).
+                Some JavaScript versions interpret numbers as octal if they are written with a leading zero.
+
+                2. By default, JavaScript displays numbers as base 10 decimals.
+
+                3. But you can use the toString() method to output numbers from base 2 to base 36.
+
+*/
+
+let myNumber = 32;
+console.log(myNumber.toString(32)); // 10
+console.log(myNumber.toString(16)); // 20
+console.log(myNumber.toString(2)); // 100000
+
+
+// JavaScript Numbers as Objects -- Normally JavaScript numbers are primitive values created from literals:
+//                                  But numbers can also be defined as objects with the keyword new:
+
+let y = new Number(123);
+console.log(y);
+
+/*
+
+Do not create Number objects.
+The new keyword complicates the code and slows down execution speed.
+Number Objects can produce unexpected results:
+
+
+*/
+
+let x1 = 500;
+let y1 = new Number(500);
+console.log(x1 == y1); // true
+console.log(x1 === y1); // false
+
+let k = new Number(300)
+let j = new Number(300)
+
+// Comparing two JavaScript objects always returns false.s
+console.log(k == j); // false
+console.log(k === j); //false
