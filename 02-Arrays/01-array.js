11805c3afff1651462c80e148c3f437550c8ea8f
Manisha Varma <manishavkapex1@gmail.com>
2024-05-03 09:50:46 +0000

    Basic of Array

--- 02-Arrays/01-array.js
+++ 02-Arrays/01-array.js
@@ -1,2 +1,12 @@
+// Arrays = An array is a special variable, which can hold more than one value:
+
+/*
+
+Notes :     1. JavaScript arrays are resizable and can contain a mix of different data types. 
+            2. JavaScript arrays are zero-indexed
+            3. JavaScript array-copy operations create shallow copies rather than deep copies. (Stack and Heap)
+
+*/
+
 let myArr = [1,2,3,4,5]
 console.log(myArr[4])
