a3a426c1ae11f63b4bbdfb417493e006137da95e
Manisha Varma <manishavkapex1@gmail.com>
2024-05-07 18:33:41 +0000

    this keyword

--- 04-Functions/02-arrow-function.js
+++ 04-Functions/02-arrow-function.js
@@ -15,3 +15,40 @@ const addTwo = function(num){
     return num + 2
 }
 console.log(addTwo(6)); // 8
+
+
+
+// this keyword -- refer the current context (scope)
+
+const user = {
+    username : "Mohan",
+    price : 999,
+    welcomeMsg : function(){
+        console.log(`${this.username}, welcome Back!`); 
+
+        // Mohan, Welcome Back
+        // ------ After change username to Rohit
+        // Rohit, Welcome Back     
+        
+        console.log(this); // here in scope, this is refer to the current context
+
+        // { username : "Mohan",price : 999, welcomeMsg: [function:welcomeMsg]
+        // { username : "Rohit",price : 999, welcomeMsg: [function:welcomeMsg]
+    }
+
+    }
+
+
+user.welcomeMsg();
+
+// when change username
+user.username = "Rohit"
+user.welcomeMsg()
+console.log(this); // {}
+
+/*
+
+when you inspect in browser console.log(this), it returns window objects but here in runtime env like node, bun, dino etc 
+it retuns empty object {}
+
+*/
