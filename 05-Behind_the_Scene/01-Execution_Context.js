70fa0021b13c5399f61d94db6ad2852391aff80c
Manisha Varma <manishavkapex1@gmail.com>
2024-05-09 12:16:24 +0000

    exaple of call stack

--- 05-Behind_the_Scene/01-Execution_Context.js
+++ 05-Behind_the_Scene/01-Execution_Context.js
@@ -122,3 +122,23 @@ function three() {
 one(); // add breakpoint
 two(); // add breakpoint
 three(); // add breakpoint
+
+
+// ========================================================================
+
+function one() {
+    console.log("One")
+    two()
+}
+
+function two() {
+    console.log("two")
+    three()
+}
+
+function three() {
+    console.log("three")
+}
+one();
+two();
+three();
