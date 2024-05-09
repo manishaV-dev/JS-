91de7dab6ab9c82cede4c8eaa2b24f085fbfa596
Manisha Varma <manishavkapex1@gmail.com>
2024-05-09 07:35:32 +0000

    About IIFE

--- 04-Functions/02-arrow-function.js
+++ 04-Functions/02-arrow-function.js
@@ -140,21 +140,7 @@ console.log(subTwo(8,4)); // 4
 
 
 
-// ================================ IIFE (Immediately Invoked Function Expression) ========================
 
-
-/*
-
-NOTES : 
-
-        1. Declared functions are not executed immediately. They are "saved for later use", and will be executed later, 
-            when they are invoked (called upon).
-
-        
-        2. A JavaScript function can also be defined using an expression.
-           A function expression can be stored in a variable:
-
-*/
 
 
 
---
+++ 04-Functions/05-iife.js
@@ -0,0 +1,15 @@
+// ================================ IIFE (Immediately Invoked Function Expression) ========================
+
+
+/*
+
+NOTES : 
+
+        1. Declared functions are not executed immediately. They are "saved for later use", and will be executed later, 
+            when they are invoked (called upon).
+
+        
+        2. A JavaScript function can also be defined using an expression.
+           A function expression can be stored in a variable:
+
+*/
