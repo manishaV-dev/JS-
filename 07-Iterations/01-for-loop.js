7a74ab5fa95df4f18cec43885e865a199d438bc5
Manisha Varma <manishavkapex1@gmail.com>
2024-05-10 08:45:21 +0000

    About For Loop

---
+++ 07-Iterations/01-for-loop.js
@@ -0,0 +1,32 @@
+/*
+    A loop is a sequence of instructions that is repeated until a certain condition is met. 
+    Loops are one way to execute a statement for a variable number of times. 
+
+    There are many different kinds of loops, but they all essentially do the same thing: 
+    they repeat an action some number of times. (Note that it's possible that number could be zero!)
+
+*/
+
+
+/*
+        1. for statement : A for loop repeats until a specified condition evaluates to false. 
+                            The JavaScript for loop is similar to the Java and C for loop.
+
+                            for - loops through a block of code a number of times
+
+
+            Syntax:     for (initialization; condition; afterthought)
+                            statement
+
+                    initialization : The initializing expression initialization, if any, is executed.
+                                        is executed (one time) before the execution of the code block.
+
+                    condition : The condition expression is evaluated. If the value of condition is true, the loop 
+                                statements execute. Otherwise, the for loop terminates.
+                                defines the condition for executing the code block.
+
+                    statement: The statement executes. To execute multiple statements, use a block statement ({ }) 
+                                to group those statements. If present, the update expression afterthought is executed.
+
+                    afterthought: is executed (every time) after the code block has been executed.
+*/
