3567386b800cbc52690e2af50e512fa67e2cf0d4
Manisha Varma <manishavkapex1@gmail.com>
2024-05-10 07:54:28 +0000

    this by MDN

--- 04-Functions/04-this-keyword.js
+++ 04-Functions/04-this-keyword.js
@@ -208,3 +208,30 @@ console.log(personNew.sayName()); // undefined undefined
     To do this, the JavaScript engine uses an event loop.
 
 */
+
+
+/*
+
+        The this keyword refers to the context where a piece of code, such as a function's body, is supposed to run.
+        Most typically, it is used in object methods, where this refers to the object that the method is attached to, 
+        thus allowing the same method to be reused on different objects.
+
+        this typically refers to the global object (in non-strict mode) or undefined (in strict mode). 
+
+        Arrow functions differ in their handling of this: they inherit this from the parent scope at the time they are defined.
+        This behavior makes arrow functions particularly useful for callbacks and preserving context. However, arrow functions 
+        do not have their own this binding. Therefore, their this value cannot be set by bind(), apply() or call() methods, 
+        nor does it point to the current object in object methods.
+
+
+        Binding : In programming, a binding is an association of an identifier with a value. Not all bindings are variables — 
+                    for example, function parameters and the binding created by the catch (e) block are not "variables" in the 
+                    strict sense. 
+                                In addition, some bindings are implicitly created by the language — for example, 
+                                this and new.target in JavaScript.
+
+        A binding is mutable if it can be re-assigned, and immutable otherwise; this does not mean that the value it holds is 
+        immutable.
+
+
+*/
