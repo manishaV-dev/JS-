56ccfefdf277f2c4740ec7357b1cba2b0b9344af
Manisha Varma <manishavkapex1@gmail.com>
2024-05-13 06:23:26 +0000

    Loop for Object- for-in loop

---
+++ 07-Iterations/04-Loop_on_Objects.js
@@ -0,0 +1,7 @@
+const myObject = {
+    game1: "NFS",
+    game2: 'God of war',
+    game3: 'Witcher'
+}
+
+// For-In Loops : The JavaScript for in statement loops through the properties of an Object:
