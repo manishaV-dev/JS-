5a11c24620f491da9e2f16d5f3ca2d54fabbc37b
Manisha Varma <manishavkapex1@gmail.com>
2024-05-13 12:23:38 +0000

    More Example on forEach loop

--- 07-Iterations/05-forEach.js
+++ 07-Iterations/05-forEach.js
@@ -50,8 +50,92 @@ const coding = ['JavaScript', 'Ruby', 'Java', 'Python', 'CPP']
 // arrow Function
 
 coding.forEach((item) => {
-    console.log(item);
+    // console.log(item);
 })
+
+// Call function as a reference
+const printVal = function(val){
+    // console.log(val);
+}
+
+coding.forEach(printVal)
+
+
+coding.forEach( (val, index, arr) => {
+    // console.log(val, index, arr); // javascript 0 ['JavaScript', 'Ruby', 'Java', 'Python', 'CPP'] and so on...
+})
+
+
+const staffsDetails = [
+    { name: "Jam Josh", age: 44, salary: 4000, currency: "USD" },
+    { name: "Justina Kap", age: 34, salary: 3000, currency: "USD" },
+    { name: "Chris Colt", age: 37, salary: 3700, currency: "USD" },
+    { name: "Jane Doe", age: 24, salary: 4200, currency: "USD" }
+  ];
+
+//   staffsDetails.forEach((staff) => {
+//     let result = `I am ${staff.name}, a staff of TCS`;
+//     console.log(result); // I am Jam Josh, a staff of TCS ... and so on..
+//   })
+
+staffsDetails.forEach(({ name }, index) => {
+    let sentence = `Index ${index} : I am ${name} a staff of TCS.`;
+    // console.log(sentence); // Index 0 : I am Jam Josh, a staff of TCS ... and so on..
+  });
+
+// staffsDetails.forEach((staffDetail, index, array) => {
+//     console.log(array);
+//   });
+
+
+// Count total salary of StaffDetails
+let totalSalary = 0;
+staffsDetails.forEach(({salary}) => {
+  totalSalary += salary;
+});
+// console.log(totalSalary + " USD"); // 14900 USD
+
+
+
+// const scores = [12, 55, 70, 47];
+// let total = 0;
+// scores.forEach((addScore)=> {
+//     total += addScore
+// })
+// console.log(total); // 184
+
+
+
+
+// If staff salary >= 4000 then print there Name
+staffsDetails.forEach(({name, salary}) => {
+    if(salary >= 4000){
+    //   console.log(name);
+    }
+  });
+
+
+
+
+  const myCoding = [
+    {langName : 'Javascript', langFileName : 'js'},
+    {langName : 'Python', langFileName : 'py'},
+    {langName : 'Java', langFileName : 'Java'}
+  ]
+
+
+//   myCoding.forEach((item) => {
+//     console.log(item.langFileName); // js py java
+//     console.log(item.langName); // Javascript .....
+//   })
+
+
+myCoding.forEach(({langName, langFileName}) => {
+    console.log(`${langName} file name is ${langFileName}`);
+})
+
+
+
 
 
 
