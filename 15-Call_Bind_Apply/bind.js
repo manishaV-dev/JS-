b1c8f117a658d77afe1c0f7286ff8c4bc7037cde
Manisha Varma <manishavkapex1@gmail.com>
2024-05-29 14:38:36 +0530

    more about bind

--- 15-Call_Bind_Apply/bind.js
+++ 15-Call_Bind_Apply/bind.js
@@ -20,3 +20,81 @@ greetPerson('Hello', '!'); // Hello , Alice !
 // In this example, this inside greetPerson refers to person
 
 
+//===========================================================================================================================
+//===========================================================================================================================
+
+/*
+
+    JavaScript Function bind() --- Function Borrowing
+
+    1. With the bind() method, an object can borrow a method from another object.
+
+
+*/
+
+// 1. Example
+
+const user = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return (`${this.firstName} ${this.lastName}`)
+    }
+  }
+  
+  const member = {
+    firstName:"Hege",
+    lastName: "Nilsen",
+  }
+  
+  let userName = user.fullName.bind(member);
+  console.log(userName()); // Hege Nilsen
+
+
+  /*
+
+    Preserving this -----  Sometimes the bind() method has to be used to prevent losing this.
+
+    When a function is used as a callback, 'this' keyword is lost.
+    The bind() method solves this problem.
+
+  */
+
+    const newPerson = {
+        firstName:"John",
+        lastName: "Doe",
+        display: function () {
+            document.querySelector("#demo").innerHTML = (`${this.firstName} ${this.lastName}`)
+        }
+      }
+      
+    // setTimeout(newPerson.display, 3000); // undefined undefined
+
+    let display = newPerson.display.bind(newPerson)
+    setTimeout(display, 3000); // after 3sec John Doe
+    
+
+
+/*
+
+What is this?
+In JavaScript, the this keyword refers to an object.
+
+The this keyword refers to different objects depending on how it is used:
+
+        - In an 'object' method, 'this' refers to the 'object'.
+
+        - Alone, 'this' refers to the 'global object'.
+
+        - In a 'function', 'this' refers to the 'global object'.
+
+        - In a 'function', in strict mode, 'this' is undefined.
+
+        - In an 'event', 'this' refers to the element 'that received the event'.
+
+        - Methods like call(), apply(), and bind() can refer 'this' to any object.
+
+Note: 
+        'this' is not a variable. It is a keyword. You cannot change the value of this.
+
+*/
